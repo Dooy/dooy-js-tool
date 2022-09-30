34c5b9c4d70f1740d4029fc1bf8cb00aa27091fc
Dooy <dooy520@qq.com>
2022-10-01 01:26:16 +0800

    init

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
-// import m from "./index.js";
+// import m from "./dooy-js-tool.js";
 //const userApi = require("./index")
 // const productApi = require("./product")
-const dooy = require("./index")
-//import dooy from "./index"
+const dooy = require("./dooy-js-tool")
 
-
-//getProduct('abdd');
-//console.log(m);
 
 console.log( dooy.dateFormat(new Date(),"yyyy-MM-dd hh:mm:ss"));
 console.log( dooy.timeToDate( 1661656271,"yyyy-MM-dd hh:mm:ss"));
 console.log( dooy.nowStr( 1664400496));
-console.log( dooy.getQueryStr("abc")); // ?abc=123
+//是适合在浏览器下使用
+//console.log( dooy.getQueryStr("abc")); // ?abc=123
 
